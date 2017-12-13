07eff7a32230e75ec8967e82dc5c2b87b885ffca
justcallmeseamus <32379652+justcallmeseamus@users.noreply.github.com>
2017-12-12 17:01:23 -1000

    added more create files

--- assets/js/play.js
+++ assets/js/play.js
@@ -21,6 +21,34 @@ var playState = {
     },
 
     create: function() { 
+        //this is the physics function/options
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        //this is our pipe group
+        this.pipes = game.add.group();
+        //our timer 
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        //add bird to the stage 
+        this.bird = game.add.sprite(100, 245, 'bird');
+        //enable physics on bird
+        game.physics.arcade.enable(this.bird);
+        this.bird.body.gravity.y = 1000;
+        //feel free to tweak the 1000 gravity value
+        
+        //anchor position
+        this bird.anchor.setTo(-0.2,0.5);
+        
+        //creat keyboard input listener 
+        //create jump key
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
+        spaceKey.onDown.add(this.jump, this);
+        game.input.onDown.add(this.jump,this)
+        
+        this.score = 0;
+        this.labelScore = game.add.text( 20, 20, "0")
+        // add jump sound
+        this.jumpSound = game.add.audio('jump');
+        this.jumpSound.volume = 0.2;
+        
         
     },
 
